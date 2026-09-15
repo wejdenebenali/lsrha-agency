58a833a6fd6d385fe192052a101436d5766205a3
wejdene ben ali <benaliwejdene@gmail.com>
2026-09-15 13:52:29 +0200

    Remove blog from sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -20,12 +20,7 @@ export default function sitemap() {
       changeFrequency: "monthly",
       priority: 0.8,
     },
-    {
-      url: `${baseUrl}/blog`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.7,
-    },
+    
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
